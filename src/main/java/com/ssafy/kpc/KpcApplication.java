2967c140239c676188c95a04647bb81fd1500c4d
sycha11 <sycha112@naver.com>
2023-10-25 23:26:27 +0900

    init

---
+++ src/main/java/com/ssafy/kpc/KpcApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.kpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/kpc/KpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.kpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
